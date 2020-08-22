3e0da7220e85c9499ee0b0e68753ed09e1d83e74
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-08-21 21:17:08 -0500

    Deleted proxy

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,16 @@ const routes = {}
 routes.apiDomain = "https://agile-coast-82296.herokuapp.com";
 
 // Auth
-routes.authGoogle = `/auth/google`;
-routes.authFacebook = `/auth/facebook`;
-routes.logout = `/auth/logout`;
+routes.authGoogle = `https://agile-coast-82296.herokuapp.com/auth/google`;
+routes.authFacebook = `https://agile-coast-82296.herokuapp.com/auth/facebook`;
+routes.logout = `https://agile-coast-82296.herokuapp.com/auth/logout`;
 
 // Data
-routes.getUser = `/user`;
-routes.getProfileDataRoute = `/data/profile`;
-routes.getMapDataRoute = `/data/map`;
-routes.getFoundDogDataRoute = `/data/found`;
-routes.getBuyDataRoute = `/data/buy`;
-routes.getAdoptDataRoute = `/data/adopt`;
+routes.getUser = `https://agile-coast-82296.herokuapp.com/user`;
+routes.getProfileDataRoute = `https://agile-coast-82296.herokuapp.com/data/profile`;
+routes.getMapDataRoute = `https://agile-coast-82296.herokuapp.com/data/map`;
+routes.getFoundDogDataRoute = `https://agile-coast-82296.herokuapp.com/data/found`;
+routes.getBuyDataRoute = `https://agile-coast-82296.herokuapp.com/data/buy`;
+routes.getAdoptDataRoute = `https://agile-coast-82296.herokuapp.com/data/adopt`;
 
 module.exports = routes;
