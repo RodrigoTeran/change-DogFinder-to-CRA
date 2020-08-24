a948aaa32e727aac5dd5eaa198649593f6d4637f
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-08-24 09:32:58 -0500

    I deleted proxy

--- src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-// Domain
-const { apiDomain } = require("./routes/index")
-
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(createProxyMiddleware("/auth", { target: `${apiDomain}` }));
-};
