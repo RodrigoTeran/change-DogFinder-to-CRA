2aa5c0a57a6486d8742b62e30b0ca915cae5cc1f
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-08-24 19:27:11 -0500

    Added loaders

--- src/components/Menus/TopMenuBar.jsx
+++ src/components/Menus/TopMenuBar.jsx
@@ -220,8 +220,8 @@ const Menubar = ({
         </div>
       </nav>
       {firstResponseUserAPI ? (<></>) : (<>
-        <div style={{ color: "#FFF", display: "flex", justifyContent: "center", paddingLeft: "60px", paddingRight: "60px", alignItems: "center", width: "100%", height: "90px", position: "absolute", top: 0, zIndex: 9999999999 }}>
-          <div style={{ fontSize: "20px", fontFamily: "Roboto Condensed" }}>conectando...</div>
+        <div className="loader-menubar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" /></svg>
         </div>
       </>)}
     </>
--- src/pages/encontrada.jsx
+++ src/pages/encontrada.jsx
@@ -57,9 +57,15 @@ const DogFounded = ({
         <meta name="description" content={`Sección para que ${username} registre una mascota perdida en ${APP_NAME}`} />
       </Helmet>
       {yesRedirect ? (<Redirect to="/"></Redirect>) : (<></>)}
-      <div className={`dog-founded-page text-center`}>
-        Sección para que {username} registre a un perro perdido que encontró
+      {username ? (
+        <div className={`dog-founded-page text-center`}>
+          Sección para que {username} registre a un perro perdido que encontró
         </div>
+      ) : (
+          <div className="loader-pages">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" /></svg>
+          </div>
+        )}
       <FooterLayout styleForm="with-absolute"></FooterLayout>
     </>
   );
