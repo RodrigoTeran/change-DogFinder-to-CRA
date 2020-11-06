1fba02045a5b824e6b1f0b340fe148e90cdfc09c
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-11-06 12:17:18 -0600

    Custom Stripe checkout finished

--- src/components/BuyPageComponents/PaymentComponent/MainSectionPayment.jsx
+++ src/components/BuyPageComponents/PaymentComponent/MainSectionPayment.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import StripeCheckout from "react-stripe-checkout";
 import { Redirect } from "react-router-dom";
+
 // Selectores
 import {
   getAuth,
   getKeyActiveUser
 } from "../../../store/reducers/user/selector";
 
-import { APP_NAME, STRIPE_KEY } from "../../../utils/config";
+import { APP_NAME } from "../../../utils/config";
+import Stripe from "../Stripe";
 
 // Routes
 import { postPayment, postKeyPayment } from "../../../routes/index";
@@ -77,7 +78,7 @@ const MainSectionPayment = ({
       if (data.status === "Success") {
         // Poner contenedor email
         updateKeyActiveUser(data.email);
-        window.scroll({ top: getTop(".input-key-pay") - 110, left: 0, behavior: 'smooth' }); // Movemos el scroll para que cheque el input
+        window.scroll({ top: getTop(".input-key-pay-rect") - 110, left: 0, behavior: 'smooth' }); // Movemos el scroll para que cheque el input
       } else { // No user
         updateFailureMessagesComponent({
           state: true,
@@ -154,6 +155,9 @@ const MainSectionPayment = ({
       {redirectPaymentSuccess ? (<Redirect to="/perfil"></Redirect>) : (<></>)}
       <svg className="icon-header-section-1-index" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 224c-79.41 0-192 122.76-192 200.25 0 34.9 26.81 55.75 71.74 55.75 48.84 0 81.09-25.08 120.26-25.08 39.51 0 71.85 25.08 120.26 25.08 44.93 0 71.74-20.85 71.74-55.75C448 346.76 335.41 224 256 224zm-147.28-12.61c-10.4-34.65-42.44-57.09-71.56-50.13-29.12 6.96-44.29 40.69-33.89 75.34 10.4 34.65 42.44 57.09 71.56 50.13 29.12-6.96 44.29-40.69 33.89-75.34zm84.72-20.78c30.94-8.14 46.42-49.94 34.58-93.36s-46.52-72.01-77.46-63.87-46.42 49.94-34.58 93.36c11.84 43.42 46.53 72.02 77.46 63.87zm281.39-29.34c-29.12-6.96-61.15 15.48-71.56 50.13-10.4 34.65 4.77 68.38 33.89 75.34 29.12 6.96 61.15-15.48 71.56-50.13 10.4-34.65-4.77-68.38-33.89-75.34zm-156.27 29.34c30.94 8.14 65.62-20.45 77.46-63.87 11.84-43.42-3.64-85.21-34.58-93.36s-65.62 20.45-77.46 63.87c-11.84 43.42 3.64 85.22 34.58 93.36z" /></svg>
       <div>
+        <div className="purchase-page-payment-title">
+          Pagar una cuenta Premium de por vida
+        </div>
         {auth ? (
           <>
             {isPremium ? (
@@ -169,21 +173,7 @@ const MainSectionPayment = ({
               </>
             ) : (
                 <>
-                  <StripeCheckout
-                    stripeKey={STRIPE_KEY}
-                    token={makePayment}
-                    name={`Compra plan premium`}
-                    amount={product.price * 100}
-                    currency="MXN"
-                  >
-                    <ButtonWhiteRectangle text={`Ser premium por $${product.price}`}
-                      width="275px"
-                      height="50px"
-                      mt="mt-0"
-                      noClick={true}
-                    ></ButtonWhiteRectangle>
-                  </StripeCheckout>
-                  <div>
+                  <div className="input-key-pay-rect">
                     {keyActiveUser ? (
                       <>
                         <div className="input-key-pay">
@@ -196,7 +186,11 @@ const MainSectionPayment = ({
                           </div>
                         </div>
                       </>
-                    ) : (<></>)}
+                    ) : (<>
+                      <Stripe
+                        makePayment={makePayment}
+                      ></Stripe>
+                    </>)}
                   </div>
                   {isLoading ? (
                     <div className="loader-block" style={{
