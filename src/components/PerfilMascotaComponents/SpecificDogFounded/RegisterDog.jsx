cef75301ead3f2886c329ac4969fa9a98dfcba55
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-11-29 11:17:21 -0600

    Added loader of register dog founded

--- src/components/PerfilMascotaComponents/SpecificDogFounded/RegisterDog.jsx
+++ src/components/PerfilMascotaComponents/SpecificDogFounded/RegisterDog.jsx
@@ -25,7 +25,9 @@ const RegisterDog = ({
   updateSuccessMessagesComponent
 }) => {
   const [yesInstructions, setInstructions] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const registerDogFounded = () => {
+    setIsLoading(true);
     fetch(`${editProfileDogFoundedRegistration}/${petProfile.name}`, {
       method: "PUT",
       credentials: "include",
@@ -43,6 +45,7 @@ const RegisterDog = ({
        * "already" = ya estaba
        * "mucho texto... xd" = se necesitan mas requirements
        */
+      setIsLoading(false);
       if (data.status === "no") {
         updateFailureMessagesComponent({
           state: true,
@@ -166,6 +169,14 @@ const RegisterDog = ({
           </div>
         </div>
       </div>
+      {isLoading ? (
+        <div className="loader-block" style={{
+          paddingTop: "80px",
+          paddingBottom: "60px"
+        }}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" /></svg>
+        </div>
+      ) : (<></>)}
     </div>
   )
 };
