d85dba8651fdb0f1ac82904470ce3509d2e05fbe
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-12-03 09:37:13 -0600

    I added the location component

--- src/components/ProfilePageComponents/CompanyInfo.jsx
+++ src/components/ProfilePageComponents/CompanyInfo.jsx
@@ -7,6 +7,7 @@ import {
 import ChangeLogoCompany from "./CompanyMethods/ChangeLogoCompany";
 import ChangeNameCompany from "./CompanyMethods/ChangeNameCompany";
 import ChangeWebPageCompany from "./CompanyMethods/ChangeWebPageCompany";
+import ChangeLocationCompany from "./CompanyMethods/ChangeLocationCompany";
 
 const CompanyInfo = ({
   userCompany
@@ -42,8 +43,7 @@ const CompanyInfo = ({
           <div onClick={() => { setInstructions(!yesInstructions) }} title="Instrucciones" style={{
             display: "flex",
             alignItems: "center",
-            justifyContent: `${
-              window.innerWidth < 768 ? (yesInstructions ? ("left") : ("center")) : ("left")
+            justifyContent: `${window.innerWidth < 768 ? (yesInstructions ? ("left") : ("center")) : ("left")
               }`
           }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z" /></svg> Instrucciones
@@ -68,6 +68,9 @@ const CompanyInfo = ({
           <ChangeWebPageCompany
             userCompany={userCompany}
           ></ChangeWebPageCompany>
+          <ChangeLocationCompany
+            userCompany={userCompany}
+          ></ChangeLocationCompany>
         </div>
       </div>
     </div>
