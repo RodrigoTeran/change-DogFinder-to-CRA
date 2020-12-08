935e3b0fe03d0829523655b58ff25457a73dd685
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-12-08 14:58:11 -0600

    Added style to the map page

--- src/components/MapPage/InputMap.jsx
+++ src/components/MapPage/InputMap.jsx
@@ -26,7 +26,7 @@ import mapStyles from "../../utils/mapStyles";
 
 const mapContainerStyle = {
   width: "100%",
-  height: window.innerWidth < 1121 ? ("calc(100vh - 300px)") : ("calc(100vh - 100px)"),
+  height: "100%",
 };
 const options = {
   disableDefaultUI: true,
@@ -37,7 +37,8 @@ const options = {
 const InputMap = ({
   petProfile
 }) => {
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [isButtonsContainerOpen, setButtonsContainerOpen] = useState(false);
   const [actualCoordenates, setActualCoordenates] = useState({
     lat: 0,
     lng: 0
@@ -73,12 +74,63 @@ const InputMap = ({
   }, []);
 
   return (
-    <>
-      <div>
-        <Search
-          panTo={panTo}
-          actualCoordenates={actualCoordenates}
-        ></Search>
+    <div className="map-page-right">
+      {isLoading ? (
+        <div className="loader-mapRightColumn">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" /></svg>
+        </div>
+      ) : (<></>)}
+      <Search
+        panTo={panTo}
+        actualCoordenates={actualCoordenates}
+      ></Search>
+      <div className={`map-page-right-buttons-container ${isButtonsContainerOpen ? ("open") : ("close")}`}>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setActualCoordenates({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              });
+              panTo({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            },
+            () => null
+          );
+        }}>
+          Poner en mi ubicación actual
+        </button>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+        }}>
+          Ver mis propios perfiles
+        </button>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+        }}>
+          Ver perros encontrados
+        </button>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+        }}>
+          Ver perros perdidos
+        </button>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+        }}>
+          Ver perros organizaciones
+        </button>
+        <button className="map-page-right-buttons-container-buttons" onClick={() => {
+          setButtonsContainerOpen(false);
+        }}>
+          Ver perros encontrados por mí
+        </button>
+        <button className="map-page-right-buttons-container-giant-button" onClick={() => { setButtonsContainerOpen(!isButtonsContainerOpen) }}>
+          <svg className={`${isButtonsContainerOpen ? ("open") : ("close")}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" /></svg>
+        </button>
       </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
@@ -89,7 +141,7 @@ const InputMap = ({
         onLoad={onMapload}
       >
       </GoogleMap>
-    </>
+    </div>
   );
 };
 
--- src/components/MapPage/RightColumnMapPage.jsx
+++ src/components/MapPage/RightColumnMapPage.jsx
@@ -1,24 +1,13 @@
-import React, { } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import InputMap from "./InputMap";
 
-import {
-  getPetProfile
-} from "../../store/reducers/user/selector";
-
-const RightColumnMapPage = ({
-  petProfile
-}) => {
+const RightColumnMapPage = () => {
   return (
-    <InputMap></InputMap>
+    <>
+      <InputMap>
+      </InputMap>
+    </>
   )
 }
 
-// Clases de REDUX
-const mapStateToProps = (state) => {
-  return {
-    petProfile: getPetProfile(state)
-  };
-};
-
-export default connect(mapStateToProps)(RightColumnMapPage);
+export default RightColumnMapPage;
