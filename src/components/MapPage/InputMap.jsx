edcb520779f61c2b102fdf1d8b0704b0858e660b
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-12-09 17:30:07 -0600

    Added title map page

--- src/components/MapPage/InputMap.jsx
+++ src/components/MapPage/InputMap.jsx
@@ -326,6 +326,36 @@ const InputMap = ({
           <svg className={`${isButtonsContainerOpen ? ("open") : ("close")}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" /></svg>
         </button>
       </div>
+      <div className="which-view-active-map-page">
+        {whichViewIsActive === "ownPremiumProfiles" ? (
+          <>
+            {/*ownPremiumProfiles*/}
+            Mis perfiles
+          </>
+        ) : (whichViewIsActive === "globalPremiumProfiles" ? (
+          <>
+            {/*globalPremiumProfiles*/}
+            Perros perdidos
+          </>
+        ) : (whichViewIsActive === "globalDogsFounded" ? (
+          <>
+            {/*globalDogsFounded*/}
+            Perros encontrados
+          </>
+        ) : (whichViewIsActive === "ownDogsFounded" ? (
+          <>
+            {/*ownDogsFounded*/}
+            Mis perros encontrados
+          </>
+        ) : (whichViewIsActive === "globalCompanies" ? (
+          <>
+            {/*globalCompanies*/}
+            Compañías
+          </>
+        ) : (
+            <></>
+          )))))}
+      </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={12}
