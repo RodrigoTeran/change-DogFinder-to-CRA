992ab73b13ce51bc5f2bc1b6e9f40a3ee70c3ec9
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-01-09 18:20:59 -0600

    Finished everything

--- src/components/NotificationsPageComponents/Col1SmartComponent.jsx
+++ src/components/NotificationsPageComponents/Col1SmartComponent.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ButtonWhiteRectangle from "../Buttons/ButtonWhiteRectangle";
-import { deleteJarvisFromUser } from "../../routes/jarvisRoutes";
+import {
+  deleteJarvisFromUser,
+  updateAnswerIAJarvis,
+} from "../../routes/jarvisRoutes";
 import { updatePushJarvisInfoAction } from "../../store/reducers/jarvis/actions";
 import {
   updateFailureMessagesComponentAction,
@@ -47,13 +50,51 @@ const Col1SmartComponent = ({
       });
     }
   }, [bannerOkCancelAction]);
-  
+
   const askIfWantDelete = () => {
     updateBannerOkCancelAction({
       fromWho: TEXT_WANT_DELETE_NOTIFICATION,
       inLayout: true,
       okButton: false,
     });
+  };
+
+  const updateJarvis = (answer) => {
+    setIsLoading(true);
+    const body = {
+      idJarvis,
+      answer,
+    };
+    fetch(updateAnswerIAJarvis, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        token: localStorage.getItem("token"),
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        if (data.status === "true") {
+          updatePushJarvisInfo(data.arrayJarvisesWithInfo);
+          updateSuccessMessagesComponent({
+            state: true,
+            title: "Notificación actualizada",
+            description: "Notificación actualizada exitosamente.",
+          });
+        } else {
+          updateFailureMessagesComponent({
+            state: true,
+            title: "Error",
+            description: "No se pudo actualizar la notificación",
+          });
+        }
+      });
   };
 
   const deleteUserInJarvis = () => {
@@ -103,6 +144,9 @@ const Col1SmartComponent = ({
   var idJarvis = undefined;
   var typeUser = undefined;
 
+  var isArtesanal = undefined;
+  var numeroIA = undefined;
+
   const getJarvisInfo = () => {
     if (inWhatView === "viewIAUser") {
       if (jarvisesIAUser.length > 0) {
@@ -134,6 +178,9 @@ const Col1SmartComponent = ({
       typeUser = jarvises[indexJarvis].typeUser;
 
       createdAt = formatDate(jarvises[indexJarvis].createdAt);
+      numeroIA = jarvises[indexJarvis].answerIAJarvis;
+      isArtesanal = jarvises[indexJarvis].artesanal;
+
       if (inWhatView === "viewIAUser") {
         numeroView = viewIAUser + 1;
       } else if (inWhatView === "viewManualUser") {
@@ -143,9 +190,15 @@ const Col1SmartComponent = ({
       } else if (inWhatView === "viewManualCompany") {
         numeroView = viewManualCompany + 1;
       }
-      document.querySelector(
-        ".main-section-notificaciones-page-container-hr"
-      ).style.height = "420px";
+      if (!isArtesanal && numeroIA === 0) {
+        document.querySelector(
+          ".main-section-notificaciones-page-container-hr"
+        ).style.height = "630px";
+      } else {
+        document.querySelector(
+          ".main-section-notificaciones-page-container-hr"
+        ).style.height = "420px";
+      }
     } else {
       try {
         document.querySelector(
@@ -195,26 +248,91 @@ const Col1SmartComponent = ({
         <></>
       ) : (
         <div className="main-section-notificaciones-page-container-col-1-smart-delete">
-          <ButtonWhiteRectangle
-            text="Borrar esta notificación"
-            width="100%"
-            height="50px"
-            redDif="redColor-2"
-            clickFunctionAnotherOne={() => {
-              if (!isLoading) {
-                askIfWantDelete();
-              }
-            }}
-          >
-            <svg
-              width="20px"
-              height="20px"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 352 512"
-            >
-              <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
-            </svg>
-          </ButtonWhiteRectangle>
+          {isArtesanal || (numeroIA === 1 && !isArtesanal) ? (
+            <div>
+              {!isArtesanal && typeUser === "owner" ? (
+                <div style={{ marginTop: "30px" }}>
+                  Cuando elimines esta notificación lo mejor es poner a tu
+                  perfil de tu mascota como en casa para que no te vuelva a
+                  mandar la notificación.
+                </div>
+              ) : (
+                <></>
+              )}
+              <ButtonWhiteRectangle
+                text="Borrar esta notificación"
+                width="100%"
+                height="50px"
+                redDif="redColor-2"
+                clickFunctionAnotherOne={() => {
+                  if (!isLoading) {
+                    askIfWantDelete();
+                  }
+                }}
+              >
+                <svg
+                  width="20px"
+                  height="20px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 352 512"
+                >
+                  <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+                </svg>
+              </ButtonWhiteRectangle>
+            </div>
+          ) : (
+            <div style={{ marginTop: "30px" }}>
+              Para ver la información de contacto del otro usuario/empresa...
+              primero tienes que especificar lo siguiente.
+              <br />
+              <br />
+              <span style={{ color: "#CCC" }}>
+                Si ya no llegas a ver este mensaje es porque el otro
+                usuario/empresa ya calificó a la IA.
+              </span>
+              <ButtonWhiteRectangle
+                text="La IA acertó"
+                width="100%"
+                height="50px"
+                greenDif="greenColor"
+                clickFunctionAnotherOne={() => {
+                  if (!isLoading) {
+                    updateJarvis(true);
+                  }
+                }}
+              >
+                <svg
+                  width="20px"
+                  height="20px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
+                </svg>
+              </ButtonWhiteRectangle>
+              <ButtonWhiteRectangle
+                text="La IA no acertó"
+                width="100%"
+                height="50px"
+                redDif="redColor"
+                mt="mt-2"
+                clickFunctionAnotherOne={() => {
+                  if (!isLoading) {
+                    updateJarvis(false);
+                  }
+                }}
+              >
+                <svg
+                  width="20px"
+                  height="20px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 352 512"
+                >
+                  <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+                </svg>
+              </ButtonWhiteRectangle>
+            </div>
+          )}
         </div>
       )}
       {isLoading ? (
--- src/routes/jarvisRoutes.js
+++ src/routes/jarvisRoutes.js
@@ -6,6 +6,9 @@ routes.apiDomain = process.env.REACT_APP_API_DOMAIN;
 // POST
 routes.createMatchJarvis = `${routes.apiDomain}/jarvis/create/match`;
 
+// PUT
+routes.updateAnswerIAJarvis = `${routes.apiDomain}/jarvis//edit/match`;
+
 // DELETE
 routes.deleteJarvisFromUser = `${routes.apiDomain}/jarvis/delete/jarvis`;
 
