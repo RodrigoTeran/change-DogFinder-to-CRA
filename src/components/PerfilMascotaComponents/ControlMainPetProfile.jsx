4c7fa53a708431659dabc86f1ffb74d5155d575b
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-01-21 09:10:40 -0600

    Edited the style in different devices

--- src/components/PerfilMascotaComponents/ControlMainPetProfile.jsx
+++ src/components/PerfilMascotaComponents/ControlMainPetProfile.jsx
@@ -97,44 +97,55 @@ const ControlMainPetProfile = ({
         <>
           <div className={`calendar-title div-calendar-pet-founded`}>
             <span>¿Cuándo encontraste a esta mascota?</span>
-            <div
-              title={`Cambiar fecha`}
-              style={{
-                display: "grid",
-                placeContent: "center",
-              }}
-              onClick={() => {
-                setIsCalendarOpen(!isCalendarOpen);
-              }}
-            >
-              <svg
-                className="pet-profile-page-header-h1-svg"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
+            {isMobile ? null : (
+              <div
+                title={`Cambiar fecha`}
+                style={{
+                  display: "grid",
+                  placeContent: "center",
+                }}
+                onClick={() => {
+                  setIsCalendarOpen(!isCalendarOpen);
+                }}
               >
-                <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z" />
-              </svg>
-            </div>
+                <svg
+                  className="pet-profile-page-header-h1-svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                >
+                  <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z" />
+                </svg>
+              </div>
+            )}
           </div>
           <div
             className={`calendar-container div-calendar-pet-founded-2 ${
               firstAnimCalendar ? "yes" : ""
             } `}
             style={{
-              height: isCalendarOpen
+              height: isMobile
+                ? `${
+                    heightCalendar -
+                    parseInt(window.innerWidth <= 1120 ? 50 : 25)
+                  }px`
+                : isCalendarOpen
                 ? `${
                     heightCalendar -
                     parseInt(window.innerWidth <= 1120 ? 50 : 25)
                   }px`
                 : "0px",
-              marginTop: isCalendarOpen
+              marginTop: isMobile
+                ? `${window.innerWidth <= 1120 ? "20px" : "0px"}`
+                : isCalendarOpen
                 ? `${window.innerWidth <= 1120 ? "20px" : "0px"}`
                 : "0px",
-              marginBottom: isCalendarOpen ? "20px" : "0px",
+              marginBottom: isMobile ? "20px" : isCalendarOpen ? "20px" : "0px",
             }}
           >
             <div
-              className={`calendar-animation ${isCalendarOpen ? "open" : ""}`}
+              className={`calendar-animation ${
+                isMobile ? "open" : isCalendarOpen ? "open" : ""
+              }`}
             >
               <Calendar
                 className={`calendar`}
--- src/pages/perfilMascota.jsx
+++ src/pages/perfilMascota.jsx
@@ -19,25 +19,17 @@ import RegisterDog from "../components/PerfilMascotaComponents/SpecificDogFounde
 import {
   updateFailureMessagesComponentAction,
   updateTopMenuBarActivatedAction,
-  updateSuccessMessagesComponentAction
+  updateSuccessMessagesComponentAction,
 } from "../store/reducers/layout/actions";
 import DeletePerfilMascota from "../components/PerfilMascotaComponents/DeletePerfilMascota";
 
-import {
-  getPetProfile
-} from "../store/reducers/user/selector";
+import { getPetProfile } from "../store/reducers/user/selector";
 
-import {
-  updatePetProfileAction
-} from "../store/reducers/user/actions";
+import { updatePetProfileAction } from "../store/reducers/user/actions";
 
-import {
-  getpetProfileDataRoute
-} from "../routes/index";
+import { getpetProfileDataRoute } from "../routes/index";
 
-import {
-  getPetProfileDogFoundedProvider
-} from "../routes/indexDogFounded";
+import { getPetProfileDogFoundedProvider } from "../routes/indexDogFounded";
 
 import PerfilMascotaHeader from "../components/PerfilMascotaComponents/PerfilMascotaHeader";
 
@@ -45,14 +37,14 @@ const PerfilMascota = ({
   updateTopMenuBarActivated,
   updateFailureMessagesComponent,
   updatePetProfile,
-  petProfile
+  petProfile,
 }) => {
   let location = useLocation();
   const [yesRedirect, setYesRedirect] = useState(false);
   const [yesDataAPI, setYesDataAPI] = useState(false);
   useEffect(() => {
     handleResize();
-    getPetProfileDataFunction();  // Get data
+    getPetProfileDataFunction(); // Get data
     updateTopMenuBarActivated(true); // Para que el topMenuBar siempre esté con color
     window.addEventListener("resize", handleResize);
     return () => {
@@ -65,13 +57,16 @@ const PerfilMascota = ({
       setIsMobile(true);
     } else {
       setIsMobile(false);
-    };
+    }
   };
 
   const getURL = () => {
     const pathName = location.pathname;
     const isPetProfileXd = pathName[4];
-    const mascotaStringFeo = pathName.substr(isPetProfileXd === "f" ? (16) : (18), pathName.length - 1);
+    const mascotaStringFeo = pathName.substr(
+      isPetProfileXd === "f" ? 16 : 18,
+      pathName.length - 1
+    );
     const mascota = mascotaStringFeo.replace(/-/gi, " ");
     return mascota;
   };
@@ -81,90 +76,100 @@ const PerfilMascota = ({
     const pathName = location.pathname;
     const isPetProfileXd = pathName[4];
     if (!yesDataAPI) {
-      fetch(`${isPetProfileXd === "f" ? (`${getpetProfileDataRoute}/${getURL()}`) : (`${getPetProfileDogFoundedProvider}/${getURL()}`)}`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "token": localStorage.getItem("token"),
-          "isPetFromCompany": petProfile.isPetFromCompany ? (true) : (false)
+      fetch(
+        `${
+          isPetProfileXd === "f"
+            ? `${getpetProfileDataRoute}/${getURL()}`
+            : `${getPetProfileDogFoundedProvider}/${getURL()}`
+        }`,
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            token: localStorage.getItem("token"),
+            isPetFromCompany: petProfile.isPetFromCompany ? true : false,
+          },
         }
-      }).then(res => {
-        return res.json();
-      }).then(data => {
-        if (data.status) { // todo bien
-          setYesDataAPI(true);
-          updatePetProfile({
-            selectedState: "isPetProfile",
-            state: data.isPetProfile
-          });
-          updatePetProfile({
-            selectedState: "name",
-            state: data.profilePet.petName
-          });
-          updatePetProfile({
-            selectedState: "petProfileImage",
-            state: data.profilePet.profileImage
-          });
-          updatePetProfile({
-            selectedState: "images",
-            state: data.profilePet.images
-          });
-          updatePetProfile({
-            selectedState: "isLost",
-            state: isPetProfileXd === "f" ? (data.profilePet.isLost) : (false)
-          });
-          updatePetProfile({
-            selectedState: "dogBreed",
-            state: data.profilePet.race
-          });
-          updatePetProfile({
-            selectedState: "location",
-            state: data.profilePet.location
-          });
-          updatePetProfile({
-            selectedState: "coordenates",
-            state: data.profilePet.coordenates
-          });
-          if (isPetProfileXd !== "f") {
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          if (data.status) {
+            // todo bien
+            setYesDataAPI(true);
             updatePetProfile({
-              selectedState: "isRegistered",
-              state: data.profilePet.isRegistered
+              selectedState: "isPetProfile",
+              state: data.isPetProfile,
             });
-          };
-          updatePetProfile({
-            selectedState: "size",
-            state: data.profilePet.size
-          });
-          updatePetProfile({
-            selectedState: "isPetFromCompany",
-            state: data.profilePet.isPetFromCompany
-          });
-          updatePetProfile({
-            selectedState: "mainColor",
-            state: data.profilePet.mainColor
-          });
-          updatePetProfile({
-            selectedState: "gender",
-            state: data.profilePet.gender
-          });
-          if (data.profilePet.whenIsLost) {
             updatePetProfile({
-              selectedState: "whenIsLost",
-              state: data.profilePet.whenIsLost
+              selectedState: "name",
+              state: data.profilePet.petName,
             });
-          };
-        } else {
-          updateFailureMessagesComponent({
-            state: true,
-            title: "Error con el contacto con el perfil",
-            description: `Vuelva a intentarlo.`,
-          });
-          setYesRedirect(true);
-        };
-      });
-    };
+            updatePetProfile({
+              selectedState: "petProfileImage",
+              state: data.profilePet.profileImage,
+            });
+            updatePetProfile({
+              selectedState: "images",
+              state: data.profilePet.images,
+            });
+            updatePetProfile({
+              selectedState: "isLost",
+              state: isPetProfileXd === "f" ? data.profilePet.isLost : false,
+            });
+            updatePetProfile({
+              selectedState: "dogBreed",
+              state: data.profilePet.race,
+            });
+            updatePetProfile({
+              selectedState: "location",
+              state: data.profilePet.location,
+            });
+            updatePetProfile({
+              selectedState: "coordenates",
+              state: data.profilePet.coordenates,
+            });
+            if (isPetProfileXd !== "f") {
+              updatePetProfile({
+                selectedState: "isRegistered",
+                state: data.profilePet.isRegistered,
+              });
+            }
+            updatePetProfile({
+              selectedState: "size",
+              state: data.profilePet.size,
+            });
+            updatePetProfile({
+              selectedState: "isPetFromCompany",
+              state: data.profilePet.isPetFromCompany,
+            });
+            updatePetProfile({
+              selectedState: "mainColor",
+              state: data.profilePet.mainColor,
+            });
+            updatePetProfile({
+              selectedState: "gender",
+              state: data.profilePet.gender,
+            });
+            if (data.profilePet.whenIsLost) {
+              updatePetProfile({
+                selectedState: "whenIsLost",
+                state: data.profilePet.whenIsLost,
+              });
+            }
+          } else {
+            updateFailureMessagesComponent({
+              state: true,
+              title: "Error con el contacto con el perfil",
+              description: `Vuelva a intentarlo.`,
+            });
+            setYesRedirect(true);
+          }
+        });
+    }
   };
   return (
     <>
@@ -172,56 +177,70 @@ const PerfilMascota = ({
         <title>{`${APP_NAME} - ${petProfile.name}`}</title>
         <meta name="description" content={`Perfil de ${petProfile.name}`} />
       </Helmet>
-      {yesRedirect ? (<Redirect to="/"></Redirect>) : (<></>)}
+      {yesRedirect ? <Redirect to="/"></Redirect> : <></>}
       {yesDataAPI ? (
-        <div className={`pet-profile-page space-footer-bottom ${isMobile ? ("column-pet-profile-page") : ("row-pet-profile-page")}`}>
+        <div
+          className={`pet-profile-page space-footer-bottom ${
+            isMobile ? "column-pet-profile-page" : "row-pet-profile-page"
+          }`}
+        >
           {isMobile ? (
             <>
-              <ChangeNameProfile></ChangeNameProfile>
-              <PerfilMascotaHeader></PerfilMascotaHeader>
-              <ChangeImageProfile getURL={getURL}></ChangeImageProfile>
-              <ControlMainPetProfile isMobile={isMobile}></ControlMainPetProfile>
-              <InfoPetProfile></InfoPetProfile>
-              <AgeProfileComponent></AgeProfileComponent>
-              <MainCVComponent></MainCVComponent>
-              <DeletePerfilMascota setYesRedirectProp={() => {
-                setYesRedirect(true);
-              }} getURL={getURL}></DeletePerfilMascota>
-              {petProfile.isPetProfile ? (<></>) :
-                (
-                  <RegisterDog></RegisterDog>
-                )
-              }
-            </>
-          ) : (
-              <>
-                <div className={`pet-profile-page-1`}>
+              <div className="pet-profile-first-div-mobile-mode">
+                <div>
+                  <ChangeNameProfile></ChangeNameProfile>
                   <PerfilMascotaHeader></PerfilMascotaHeader>
                   <ChangeImageProfile getURL={getURL}></ChangeImageProfile>
                 </div>
-                <div className={`pet-profile-page-2`}>
-                  <ChangeNameProfile></ChangeNameProfile>
-                  <ControlMainPetProfile></ControlMainPetProfile>
-                  <DeletePerfilMascota setYesRedirectProp={() => {
-                    setYesRedirect(true);
-                  }} getURL={getURL}></DeletePerfilMascota>
-                  <InfoPetProfile></InfoPetProfile>
-                  <AgeProfileComponent></AgeProfileComponent>
-                  <MainCVComponent></MainCVComponent>
-                  {petProfile.isPetProfile ? (<></>) :
-                    (
-                      <RegisterDog></RegisterDog>
-                    )
-                  }
+                <div>
+                  <ControlMainPetProfile
+                    isMobile={isMobile}
+                  ></ControlMainPetProfile>
                 </div>
-              </>
-            )}
+              </div>
+              <div>
+                <InfoPetProfile></InfoPetProfile>
+                <AgeProfileComponent></AgeProfileComponent>
+                <MainCVComponent></MainCVComponent>
+                <DeletePerfilMascota
+                  setYesRedirectProp={() => {
+                    setYesRedirect(true);
+                  }}
+                  getURL={getURL}
+                ></DeletePerfilMascota>
+                {petProfile.isPetProfile ? <></> : <RegisterDog></RegisterDog>}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={`pet-profile-page-1`}>
+                <PerfilMascotaHeader></PerfilMascotaHeader>
+                <ChangeImageProfile getURL={getURL}></ChangeImageProfile>
+              </div>
+              <div className={`pet-profile-page-2`}>
+                <ChangeNameProfile></ChangeNameProfile>
+                <ControlMainPetProfile></ControlMainPetProfile>
+                <DeletePerfilMascota
+                  setYesRedirectProp={() => {
+                    setYesRedirect(true);
+                  }}
+                  getURL={getURL}
+                ></DeletePerfilMascota>
+                <InfoPetProfile></InfoPetProfile>
+                <AgeProfileComponent></AgeProfileComponent>
+                <MainCVComponent></MainCVComponent>
+                {petProfile.isPetProfile ? <></> : <RegisterDog></RegisterDog>}
+              </div>
+            </>
+          )}
         </div>
       ) : (
-          <div className="loader-pages">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" /></svg>
-          </div>
-        )}
+        <div className="loader-pages">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" />
+          </svg>
+        </div>
+      )}
       <FooterLayout styleForm="with-absolute"></FooterLayout>
     </>
   );
@@ -230,17 +249,25 @@ const PerfilMascota = ({
 // Clases de REDUX
 const mapStateToProps = (state) => {
   return {
-    petProfile: getPetProfile(state)
+    petProfile: getPetProfile(state),
   };
 };
 
 // Acciones de REDUX
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateTopMenuBarActivated: (data) => { dispatch(updateTopMenuBarActivatedAction(data)) },
-    updateFailureMessagesComponent: (data) => { dispatch(updateFailureMessagesComponentAction(data)) },
-    updatePetProfile: (data) => { dispatch(updatePetProfileAction(data)) },
-    updateSuccessMessagesComponent: (data) => { dispatch(updateSuccessMessagesComponentAction(data)) }
+    updateTopMenuBarActivated: (data) => {
+      dispatch(updateTopMenuBarActivatedAction(data));
+    },
+    updateFailureMessagesComponent: (data) => {
+      dispatch(updateFailureMessagesComponentAction(data));
+    },
+    updatePetProfile: (data) => {
+      dispatch(updatePetProfileAction(data));
+    },
+    updateSuccessMessagesComponent: (data) => {
+      dispatch(updateSuccessMessagesComponentAction(data));
+    },
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(PerfilMascota);
